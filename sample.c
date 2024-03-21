3f21c06a21bb77df8393819e20cfd3ff7e3cdb46
Anya Nguyen <113313887+anya-nguyen-dev@users.noreply.github.com>
2024-03-21 00:02:57 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -6,7 +6,13 @@
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-char cmd[256] = "wc -c < ";
-strcat(cmd, argv[1]);
+
+char *cmdargv = argv[1];  
+char cmdsize[256] = {0};
+  encodeShellString(cmdsize, 256, cmdargv); 
+
+char cmd[256] = {0};
+
+sprintf(cmd, "userinfo -v %s", cmdsize);
 system(cmd);
 }
