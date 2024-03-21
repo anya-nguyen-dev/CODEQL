f9e2b8f18e29805d2a7d465911362fd19b8563a0
Anya Nguyen <113313887+anya-nguyen-dev@users.noreply.github.com>
2024-03-20 23:46:35 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define BUFSIZE 256
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
-
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
-    
-    return 0;
+char cmd[BUFSIZE] = "wc -c < ";
+snprintf(cmd, argv[1]);
+system(cmd);
 }
