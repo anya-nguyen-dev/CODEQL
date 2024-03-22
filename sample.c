363765c74546a2447348039dcac517ae38280905
Anya Nguyen <113313887+anya-nguyen-dev@users.noreply.github.com>
2024-03-21 23:50:18 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+ 
+#define BUFFSIZE 256
 
-int main(int argc, char **argv) {
-char *items[] = {"boat", "car", "truck", "train"};
-int index = atoi(argv[1]);
-printf("You selected %s\n", items[index-1]);
+int read_file(const char *filename) {
+FILE* ptr = fopen(filename, "r");
+
+if (ptr == NULL) {
+printf("File cannot be opened.\n");
+return(-1);
+}
+
+char buff[BUFFSIZE];
+
+while (fgets(buff, BUFFSIZE, ptr) != NULL) {
+printf("%s", buff);
+}
+
+return 0; 
 }
